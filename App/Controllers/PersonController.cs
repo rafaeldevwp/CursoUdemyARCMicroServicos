5c65362be9251cd73f3b7a82e243cbec7122c19b
rafaeldevwp <fael.loliveira@outlook.com>
2023-03-30 13:42:07 -0300

    Inclusao de controller pessoa

---
+++ App/Controllers/PersonController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace App.Controllers;
+
+[ApiController]
+[Route("Api/v1/Person")]
+public class PersonController : ControllerBase
+{
+
+
+[HttpGet]
+public async Task<IActionResult> Get(){
+
+    var person = new Person{
+        Id = 1,
+        Name = "Rafael",
+        Address = "Rua Antonio Rodrigues da Costa - Casa Branca"
+      };
+
+    return Ok(person);
+}
+
+[HttpPost]
+[ProducesResponseType(typeof(Person),204)]
+public async Task<IActionResult> Post([FromBody] Person person){
+
+    if(person is null) return BadRequest();
+
+    IList<Person> persons = new List<Person>();
+    persons.Add(person);
+
+    return Ok(person);
+}
+
+
+
+
+}
